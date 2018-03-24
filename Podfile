b101b151a2146f09f427042d3186253b5bd50550
Brian Chung <hei@9gag.com>
2018-03-24 09:34:21 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'ReactiveCocoaPlayground' do
+  
+	pod 'Alamofire', '~> 4.7'
+	pod 'ReactiveCocoa', '~> 7.0'
+
+  	target 'ReactiveCocoaPlaygroundTests' do
+		inherit! :search_paths
+		pod 'Quick', '~> 1.2.0'
+		pod 'Nimble', '~> 7.0.2'
+  	end
+
+end
---
+++ ReactiveCocoaPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveCocoaPlayground
+//
+//  Created by Brian Chung on 24/3/2018.
+//  Copyright © 2018 9GAG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
